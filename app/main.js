17c650dd9cfa93f619fff5f9d0540146457b0a62
MDCDEV <me@mdcdev.me>
2024-03-30 11:06:09 -0500

    second stage

--- app/main.js
+++ app/main.js
@@ -1,12 +1,20 @@
+// Require dependencies
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-// console.log("Logs from your program will appear here!");
+const CRLF = '\r\n'
 
-// Uncomment this block to pass the first stage
-const server = net.createServer((connection) => {
-  // Handle connection
+// Handle connection
+const server = net.createServer((socket) => {
+  // Log when a client connects, and log the client address
+  console.log("Client connected", socket.remoteAddress);
+
+  // Handle ping
+  socket.on("data", (data) => {
+    if (data.toString().trim().toUpperCase().includes("PING")) {
+      console.log("Received PING from client", socket.remoteAddress);
+      socket.write("PONG" + CRLF);
+    }
+  });
 });
-//
 
 server.listen(6379, "127.0.0.1");
